cfc655b2cd31d5a3e0963ad2963bd9c461876e22
TrDucChinh <chinhdevptit@gmail.com>
2024-10-16 21:24:48 +0700

    fix :<

---
+++ app/src/main/java/com/proptit/btl_oop/model/User.kt
@@ -0,0 +1,9 @@
+package com.proptit.btl_oop.model
+
+data class User(
+    val id: String = "",
+    val email: String = "",
+    val name: String = "",
+    val favoriteItems: List<String> = emptyList(),  // Rỗng ban đầu
+//    val orderHistory: List<Order> = emptyList()     // Rỗng ban đầu
+)
